841632874cb75bed88ea5b2f8a930a03c709ad19
jamesdsmith99 <36130181+jamesdsmith99@users.noreply.github.com>
2020-04-08 11:45:44 +0100

    added heatmap maker

---
+++ inference/__init__.py
@@ -0,0 +1,2 @@
+from heatmap_maker import HeatmapMaker
+from frames_to_vid import frames_to_vid
---
+++ inference/frames_to_vid.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+import os
+import re
+
+def fname_to_int(fname):
+    return int(re.sub('\D', '', fname))
+
+def frames_to_vid(image_folder, output_name):
+    images = [img for img in os.listdir(image_folder) if img.endswith('.png')]
+    images.sort(key=fname_to_int)
+
+    frame = cv.imread(os.path.join(image_folder, images[0]))
+    height, width, _ = frame.shape 
+
+    FOURCC = cv.VideoWriter_fourcc(*'MP4V')
+    video = cv.VideoWriter(output_name, FOURCC, 30, (width, height))
+
+    for img in images:
+        video.write(cv.imread(os.path.join(image_folder, img)))
+    
+    video.release()
