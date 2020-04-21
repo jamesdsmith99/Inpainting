0fedd7d365fcd4001841215b3209caa7cab401b6
jamesdsmith99 <36130181+jamesdsmith99@users.noreply.github.com>
2020-04-21 17:01:15 +0100

    added new trainers to trainers package

--- trainers/__init__.py
+++ trainers/__init__.py
@@ -1,3 +1,6 @@
+from .AcaiTrainer import AcaiTrainer
 from .AdvTrainer import AdvTrainer
+from .OrigSkipTrainer import OrigSkipTrainer
+from .RecTrainer import RecTrainer
 from .SizeTrainer import SizeTrainer
 from .SkipTrainer import SkipTrainer
